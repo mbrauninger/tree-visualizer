cceb5e7e74cd9accf05c24d6092ecd22201cb225
Michael Brauninger <mbrauninger@umass.edu>
2023-11-26 23:48:35 -0800

    Added functioning animation based on pausing

--- src/BinaryTree.tsx
+++ src/BinaryTree.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Queue } from "queue-typescript";
-import Grid from "@mui/material/Grid";
-import  Table from "@mui/material/Table";
+import Select from '@mui/material/Select'
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import ScrollableTable from "./ScrollableTable";
 
 interface Node {
   value: number;
@@ -12,21 +14,22 @@ interface Node {
   y: number;
 }
 
-interface Step {
+export interface Step {
   value: number;
   state: string;
 }
 
 const BinaryTree = () => {
-  const CANVAS_WIDTH_PERCENTAGE = 90; // Set your desired width percentage
-  const CANVAS_HEIGHT_PERCENTAGE = 90; // Set your desired height percentage
-  const DISPLAY_WIDTH = window.innerWidth * CANVAS_WIDTH_PERCENTAGE / 100;
-  const DISPLAY_HEIGHT = window.innerHeight * CANVAS_HEIGHT_PERCENTAGE / 100;
-  console.log(DISPLAY_HEIGHT)
+  const CANVAS_WIDTH_PERCENTAGE = 70; // Set your desired width percentage
+  const CANVAS_HEIGHT_PERCENTAGE = 80; // Set your desired height percentage
+  const DISPLAY_WIDTH = (window.innerWidth * CANVAS_WIDTH_PERCENTAGE) / 100;
+  const DISPLAY_HEIGHT = (window.innerHeight * CANVAS_HEIGHT_PERCENTAGE) / 100;
   const DEFAULT_RADIUS = 10;
-  const DEFAULT_NUM_NODES = 15
+  const DEFAULT_NUM_NODES = 15;
   const [nodeRadius, setNodeRadius] = useState(DEFAULT_RADIUS);
   const [numNodes, setNumNodes] = useState(DEFAULT_NUM_NODES);
+  const [playing, setPlaying] = useState(false);
+  const traversalStep = useRef(-1);
 
   function calculateBaseXOffset() {
     const numLevels = Math.floor(Math.log2(numNodes));
@@ -34,8 +37,8 @@ const BinaryTree = () => {
     let coefficient = 0;
     let percentageOfScreen = 1;
     for (let i = 0; i < numLevels; i++) {
-        coefficient += percentageOfScreen;
-        percentageOfScreen /= 2;
+      coefficient += percentageOfScreen;
+      percentageOfScreen /= 2;
     }
     return (DISPLAY_WIDTH - 20 - nodeRadius) / 2 / coefficient;
   }
@@ -46,18 +49,10 @@ const BinaryTree = () => {
   }
 
   const [baseXOffset, setBaseXOffset] = useState(calculateBaseXOffset());
-  useEffect(() => {
-    let currentRadius = DEFAULT_RADIUS;
-    while (calculateBottomWidth(currentRadius) > DISPLAY_WIDTH - 20 - nodeRadius) {
-        currentRadius -= 1;
-    }
-    setNodeRadius(currentRadius);
-    setBaseXOffset(calculateBaseXOffset())
-  }, [numNodes])
 
   const generateTree = (size: number) => {
     const numLevels = Math.floor(Math.log2(numNodes));
-    const yOffset = (DISPLAY_HEIGHT - 10) / (numLevels + 1)
+    const yOffset = (DISPLAY_HEIGHT - 10) / (numLevels + 1);
     let i = 2;
     const head = {
       value: 1,
@@ -69,7 +64,6 @@ const BinaryTree = () => {
     };
     const queue = new Queue<Node>(head);
     let xOffset = baseXOffset;
-    console.log(xOffset)
     while (i < size) {
       const current = queue.dequeue();
       current.left = {
@@ -100,93 +94,11 @@ const BinaryTree = () => {
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [traversal, setTraversal] = useState<Step[]>([]);
+  const [listedTraversal, setListedTraversal] = useState<Step[]>([]);
   const [tree, setTree] = useState<Node | null>(
-    generateTree(DEFAULT_NUM_NODES)
+    generateTree(DEFAULT_NUM_NODES),
   );
-  const [selectedOption, setSelectedOption] = useState('inOrder'); // Default option
-
-  useEffect(() => {
-    if (selectedOption === 'inOrder') {
-        const x: Step[] = []; inOrderTraverse(tree, x); setTraversal(x)
-    } else if (selectedOption === 'preOrder') {
-        const x: Step[] = []; preOrderTraverse(tree, x); setTraversal(x)
-    } else if (selectedOption === 'postOrder') {
-        const x: Step[] = []; postOrderTraverse(tree, x); setTraversal(x)
-    }
-  }, [selectedOption, tree])
-
-  useEffect(() => {
-    if (!canvasRef.current || !tree) return;
-
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    const devicePixelRatio = window.devicePixelRatio || 1;
-
-    const width = canvas.clientWidth * devicePixelRatio;
-    const height = canvas.clientHeight * devicePixelRatio;
-
-    canvas.width = width;
-    canvas.height = height;
-
-    ctx.scale(devicePixelRatio, devicePixelRatio);
-
-    const drawNode = (node: Node, x: number, y: number, color: string) => {
-      ctx.beginPath();
-      ctx.arc(x, y, nodeRadius, 0, 2 * Math.PI);
-      ctx.stroke();
-      ctx.font = `${nodeRadius}px Verdana`
-      ctx.fillStyle = color;
-      ctx.fill();
-      let xTextOffset = 0;
-      if (node.value < 10) {
-        xTextOffset = x - 4 * nodeRadius / 15;
-      } else if (node.value < 100) {
-        xTextOffset = x - 7 * nodeRadius / 12;
-      }
-      if (nodeRadius > 2) {
-          ctx.fillStyle = "black"; // Reset fill style
-          ctx.fillText(
-            node.value.toString(),
-            xTextOffset,
-            y + nodeRadius / 3,
-          );
-      }
-    };
-
-    const drawTree = (node: Node | null) => {
-      if (!node) return;
-
-      let color = "white";
-      if (node.state === "highlighted") {
-        color = "yellow";
-      } else if (node.state === "processed") {
-        color = "green";
-      }
-
-      drawNode(node, node.x, node.y, color);
-
-      if (node.left) {
-        drawTree(node.left);
-        ctx.beginPath();
-        ctx.moveTo(node.x, node.y + nodeRadius);
-        ctx.lineTo(node.left.x, node.left.y - 0.25 - nodeRadius);
-        ctx.stroke();
-      }
-
-      if (node.right) {
-        drawTree(node.right);
-        ctx.beginPath();
-        ctx.moveTo(node.x, node.y + nodeRadius);
-        ctx.lineTo(node.right.x, node.right.y - 0.25 - nodeRadius);
-        ctx.stroke();
-      }
-    };
-
-    ctx.clearRect(0, 0, width, height);
-    drawTree(tree);
-  }, [tree]);
+  const [selectedOption, setSelectedOption] = useState("inOrder"); // Default option
 
   async function delay(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -196,7 +108,7 @@ const BinaryTree = () => {
     if (!node) {
       return result;
     }
-    result.push({ state: "highlighted", value: node.value });
+    result.push({ state: "visited", value: node.value });
     inOrderTraverse(node.left, result);
     result.push({ state: "processed", value: node.value });
     inOrderTraverse(node.right, result);
@@ -215,7 +127,7 @@ const BinaryTree = () => {
     if (!node) {
       return result;
     }
-    result.push({ state: "highlighted", value: node.value });
+    result.push({ state: "visited", value: node.value });
     postOrderTraverse(node.left, result);
     postOrderTraverse(node.right, result);
     result.push({ state: "processed", value: node.value });
@@ -247,18 +159,37 @@ const BinaryTree = () => {
     return null;
   };
 
-  const animate = async (traversal: Step[]) => {
-    for (const step of traversal) {
+const animate = async () => {
+    while (traversalStep.current < traversal.length - 1 && playing) {
       if (tree) {
         setTree((prevTree) => {
           const currentTree = JSON.parse(JSON.stringify(prevTree));
-          updateTreeByValue(currentTree, step.value, step.state);
-          return currentTree; // Return the updated state
+          updateTreeByValue(currentTree, traversal[traversalStep.current].value, traversal[traversalStep.current].state);
+          return currentTree;
         });
+        setListedTraversal((prevListed) => {
+            return [...prevListed, traversal[traversalStep.current]]
+        })
       }
+      traversalStep.current += 1;
       await delay(100);
     }
   };
+
+  const performStep = async() => {
+    if (traversalStep.current === traversal.length - 1) {
+        traversalStep.current = -1;
+        setPlaying(false);
+        return;
+      }
+    setListedTraversal((prevListed) => [...prevListed, traversal[traversalStep.current]])    
+    setTree((prevTree) => {
+        const currentTree = JSON.parse(JSON.stringify(prevTree));
+        updateTreeByValue(currentTree, traversal[traversalStep.current].value, traversal[traversalStep.current].state);
+        return currentTree
+    });
+      traversalStep.current += 1;
+  }
 
   const handleInput = (event: any) => {
     const inputValue = event.target.value;
@@ -267,80 +198,204 @@ const BinaryTree = () => {
     setNumNodes(numericValue); // Set to an empty string if conversion fails
   };
 
+  const handleStart = () => {
+    setListedTraversal([])
+    traversalStep.current = -1;
+    setTree(generateTree(numNodes));
+    setPlaying(true);
+  }
+
+  useEffect(() => {
+    if (!canvasRef.current || !tree) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const devicePixelRatio = window.devicePixelRatio || 1;
+
+    const width = canvas.clientWidth * devicePixelRatio;
+    const height = canvas.clientHeight * devicePixelRatio;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.scale(devicePixelRatio, devicePixelRatio);
+
+    const drawNode = (node: Node, x: number, y: number, color: string) => {
+      ctx.beginPath();
+      ctx.arc(x, y, nodeRadius, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.font = `${nodeRadius}px Verdana`;
+      ctx.fillStyle = color;
+      ctx.fill();
+      let xTextOffset = 0;
+      if (node.value < 10) {
+        xTextOffset = x - (4 * nodeRadius) / 15;
+      } else if (node.value < 100) {
+        xTextOffset = x - (7 * nodeRadius) / 12;
+      }
+      if (nodeRadius > 2) {
+        ctx.fillStyle = "black"; // Reset fill style
+        ctx.fillText(node.value.toString(), xTextOffset, y + nodeRadius / 3);
+      }
+    };
+
+    const drawTree = (node: Node | null) => {
+      if (!node) return;
+
+      let color = "white";
+      if (node.state === "visited") {
+        color = "yellow";
+      } else if (node.state === "processed") {
+        color = "green";
+      }
+
+      drawNode(node, node.x, node.y, color);
+
+      if (node.left) {
+        drawTree(node.left);
+        ctx.beginPath();
+        ctx.moveTo(node.x, node.y + nodeRadius);
+        ctx.lineTo(node.left.x, node.left.y - 0.25 - nodeRadius);
+        ctx.stroke();
+      }
+
+      if (node.right) {
+        drawTree(node.right);
+        ctx.beginPath();
+        ctx.moveTo(node.x, node.y + nodeRadius);
+        ctx.lineTo(node.right.x, node.right.y - 0.25 - nodeRadius);
+        ctx.stroke();
+      }
+    };
+
+    ctx.clearRect(0, 0, width, height);
+    drawTree(tree);
+  }, [tree]);
+
+  useEffect(() => {
+    let currentRadius = DEFAULT_RADIUS;
+    while (
+      calculateBottomWidth(currentRadius) >
+      DISPLAY_WIDTH - 20 - nodeRadius
+    ) {
+      currentRadius -= 1;
+    }
+    setNodeRadius(currentRadius);
+    setBaseXOffset(calculateBaseXOffset());
+  }, [numNodes]);
+
+  useEffect(() => {
+    if (selectedOption === "inOrder") {
+      const x: Step[] = [];
+      inOrderTraverse(tree, x);
+      setTraversal(x);
+    } else if (selectedOption === "preOrder") {
+      const x: Step[] = [];
+      preOrderTraverse(tree, x);
+      setTraversal(x);
+    } else if (selectedOption === "postOrder") {
+      const x: Step[] = [];
+      postOrderTraverse(tree, x);
+      setTraversal(x);
+    }
+  }, [selectedOption, tree]);
+
+  useEffect(() => {
+    // Start a timed loop using setInterval
+    const intervalId = setInterval(() => {
+      if (playing) {
+        performStep()
+    }
+    }, 100); // Interval time in milliseconds
+
+    // Clean-up function to stop the loop when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [playing]); // Include counter in the dependency array if needed
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: "20px",
-      }}
-    >
-      <canvas
-        ref={canvasRef}
+    <div>
+      <div
         style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <canvas
+          ref={canvasRef}
+          style={{
             width: `${CANVAS_WIDTH_PERCENTAGE}%`,
             height: `${CANVAS_HEIGHT_PERCENTAGE}vh`,
-          backgroundColor: "lightblue",
-          marginTop: "10px",
-        }}
-      />
-      <button
-        onClick={() => {
-            setTree(generateTree(numNodes))
-            const x: Step[] = [];
-          postOrderTraverse(tree, x);
-          setTraversal(x);
-          animate(traversal);
-        }}
-      >
-        Start
-      </button>
-      <button
-        onClick={() => {
-            setTree(generateTree(numNodes));
-        }}
-      >
-        Generate Tree
-      </button>
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="inOrder"
-            checked={selectedOption === 'inOrder'}
-            onChange={() => setSelectedOption('inOrder')}
-          />
-          In-Order
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="postOrder"
-            checked={selectedOption === 'postOrder'}
-            onChange={() => setSelectedOption('postOrder')}
-          />
-          Pre-Order
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="preOrder"
-            checked={selectedOption === 'preOrder'}
-            onChange={() => setSelectedOption('preOrder')}
-          />
-          Post-Order
-        </label>
+            backgroundColor: "lightblue",
+            marginTop: "5px",
+          }}
+        />
+        <ScrollableTable data={listedTraversal} />
       </div>
-      <div>
-        <label>
-          Node Count:
-          <input
-            type="number"
-            value={numNodes}
-            onChange={handleInput}
-          />
-        </label>
+      <div style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+          marginTop: "5px",
+        }}>
+            <div style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "start",
+        }}>
+            <Select
+        style={{marginRight: 5, height: 15, width: 100, fontSize: 10}}
+    value={selectedOption}
+    label="Age"
+    onChange={(event) => {setSelectedOption(event.target.value)}}
+  >
+    <MenuItem value={"inOrder"}>inOrder</MenuItem>
+    <MenuItem value={"preOrder"}>preOrder</MenuItem>
+    <MenuItem value={"postOrder"}>postOrder</MenuItem>
+  </Select>
+        <Button
+        style={{marginRight: 5, height: 15, fontSize: 10}}
+        variant="contained"
+          onClick={() => handleStart()}
+        >
+          Start
+        </Button>
+        <Button
+        style={{marginRight: 5, height: 15, fontSize: 10}}
+        variant="contained"
+          onClick={() => {
+            performStep();
+          }}
+        >
+          Click Through
+        </Button>
+        <Button
+        style={{marginRight: 5, height: 15, fontSize: 10}}
+        variant="contained"
+          onClick={() => {
+            setPlaying(!playing);
+          }}
+        >
+          Pause
+        </Button>
+        <Button
+        style={{marginRight: 5, height: 15, fontSize: 10}}
+        variant="contained"
+          onClick={() => {
+            setTree(generateTree(numNodes));
+          }}
+        >
+          Generate Tree
+        </Button>
+        </div>
+        <div>
+          <label>
+            Node Count:
+            <input type="number" value={numNodes} onChange={handleInput} />
+          </label>
+        </div>
       </div>
     </div>
   );
---
+++ src/ScrollableTable.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import { TableContainer } from "@mui/material";
+import TableHead from '@mui/material/TableHead';
+import Paper from "@mui/material/Paper";
+import { Step } from './BinaryTree';
+
+interface ScrollableTableProps {
+  data: Step[];
+}
+
+const ScrollableTable: React.FC<ScrollableTableProps> = ({ data }) => {
+  const tableRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Scroll to the bottom when new elements are added
+    if (tableRef.current) {
+      tableRef.current.scrollTop = tableRef.current.scrollHeight;
+    }
+  }, [data]);
+
+  return (
+    <TableContainer component={Paper} style={{ maxHeight: "200px", width: '150px', overflowY: "auto", marginLeft: "10px" }} ref={tableRef}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>State</TableCell>
+            <TableCell>Node</TableCell>
+            {/* Add more table headers as needed */}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((step, index) => (
+            <TableRow key={index}>
+              <TableCell>{step.state}</TableCell>
+              <TableCell>{step.value}</TableCell>
+              {/* Add more table cells as needed */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default ScrollableTable;
